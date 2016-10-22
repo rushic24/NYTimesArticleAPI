a0c1457bb32941a3b5c69702e01f8dbe6706a294
MattDMo <mattdmo@pigimal.com>
2016-10-22 14:20:27 -0400

    Should be TestCase, not TestSuite

--- NYTimesArticleAPI/tests.py
+++ NYTimesArticleAPI/tests.py
@@ -2,6 +2,6 @@ import unittest
 import os
 
 
-class NYTimesArticleAPITests(unittest.TestSuite):
+class NYTimesArticleAPITests(unittest.TestCase):
     def __init__(self):
         self.api_key = os.environ["NYT_API_KEY"]
