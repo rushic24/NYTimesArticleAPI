212a7e597645a287cb9ee2757d35b29be90a993e
Matt Morrison <mattdmo@pigimal.com>
2016-10-22 13:21:12 -0400

    import *, change __all__

--- NYTimesArticleAPI/__init__.py
+++ NYTimesArticleAPI/__init__.py
@@ -3,12 +3,12 @@ import sys
 PY_VER = sys.version_info[0]
 
 if PY_VER == 2:
-    from api import search
+    from api import *
 elif PY_VER == 3:
-    from .api import search
+    from .api import *
 else:
     raise ImportError("Only Python versions 2.7 and 3.2+ are supported")
 
 __version__ = "1.0.0"
 __author__ = "Matt Morrison @MattDMo mattdmo@mattdmo.com"
-__all__ = ["search"]
+__all__ = ["articleAPI"]
