132d160a365580d77c1f5763b1fd1ac044133bb0
MattDMo <mattdmo@pigimal.com>
2016-10-22 14:34:49 -0400

    Print message if module is run on its own

--- NYTimesArticleAPI/__init__.py
+++ NYTimesArticleAPI/__init__.py
@@ -1,5 +1,10 @@
-from nytapi import *
+from search_api import *
 
 __version__ = "1.0.0"
-__author__ = "Matt Morrison @MattDMo mattdmo@mattdmo.com"
+__author__ = "Matt Morrison (@MattDMo)"
 __all__ = ["articleAPI"]
+
+if __name__ == "__main__":
+    print("This module cannot be run on its own. Please use by running ",
+          "\"from NYTimesArticleAPI import articleAPI\"")
+    exit(0)
